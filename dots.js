0372e685acde9ce9a542f39c82e3a5258ff27766
aliciasadowski22 <alicia.sadowski@berkeley.edu>
2021-06-28 00:10:39 -0700

    drew board and created measurements

--- dots.js
+++ dots.js
@@ -1,7 +1,41 @@
+CANVAS_SIZE= 500;
+BOARD_SIZE= 10;
+SQUARE_SIZE= CANVAS_SIZE/BOARD_SIZE
 function setup() {
-  createCanvas(400, 400);
-}
+  fill(240);
+  createCanvas(CANVAS_SIZE,CANVAS_SIZE);
+  
+  fill(0);
+  // vertical lines
+  // line(x1, y1, x2, y2)
+  // line(0, 0, 0, 400);
+  // line(100, 0, 100, 400);
+  // line(200, 0, 200, 400);
+  // line(300, 0, 300, 400);
+  // line(400, 0, 400, 400); 
 
-function draw() {
-  background(0);
-ellipse(50,50,80,80);}
+  for (i=0; i<BOARD_SIZE+1; i+=1){
+    // console.log("i is" + i);
+    line(SQUARE_SIZE*i, 0, SQUARE_SIZE*i, CANVAS_SIZE);
+    
+    // line(x1, y1, x2, y2)
+    line(0, SQUARE_SIZE*i, CANVAS_SIZE, SQUARE_SIZE*i);
+
+
+  } 
+  // horizontal lines
+  // line(x1, y1, x2, y2)
+  // line(0, 400, 400, 400);
+  // line(0, 300, 400, 300);
+  // line(0, 200, 400, 200);
+  // line(0, 100, 400, 100);
+  // line(0, 0, 400, 0);
+
+}
+// function draw() {
+//   if (mouseIsPressed) {
+//     ellipse(mouseX, mouseY, 80, 80); 
+//     fill(0);
+//   } 
+// }
+
